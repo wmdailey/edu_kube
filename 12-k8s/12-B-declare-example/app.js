69a1325c824993134e43a18ee9b5b715b0c6107f
Will Dailey <wm.k.dailey@gmail.com>
2024-02-16 20:35:57 -0800

    Major changes for K8s

--- 12-k8s/12-B-declare-example/app.js
+++ 12-k8s/12-B-declare-example/app.js
@@ -6,6 +6,7 @@ app.get('/', (req, res) => {
   res.send(`
     <h1>Hello from this NodeJS app!</h1>
     <p>Try sending a request to /error and see what happens</p>
+    <p>Richard Pearlman is a steely eyed missle man</p>
   `);
 });
 
